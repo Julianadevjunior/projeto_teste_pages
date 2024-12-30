d313ba3d4e4705ece529b021ff8546724fc50433
Julianadevjunior <jucarlos.jv@gmail.com>
2024-12-30 20:25:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ for id, caminho in enumerate(caminhos):
     if id == 0:
         pages.append(st.Page(page=caminho, title=f"ID:{bd['id'].loc[0]} Fotos "))
     else:
-        pages.append(st.Page(page=caminho, title=f"ID:{bd['id'].loc[id-1]} »»» {st.markdown('**VER FOTOS**', unsafe_allow_html=True)}"))
+        pages.append(st.Page(page=caminho, title=f"ID:{bd['id'].loc[id-1]} »»» VER FOTOS"))
 
 
 pg = st.navigation(pages, position='hidden', expanded=False)
@@ -73,7 +73,7 @@ for i in range(0, len(images), colunas_por_linha):
                     st.markdown(f"""<b style='font-size:20px'>{bd['area'].loc[cont]:.0f}m²</b> 📐<b style='font-size:10px'>ÁREA.</b>""", unsafe_allow_html=True)
                     st.markdown(f"""<b style='font-size:20px'>{bd['banheiro'].loc[cont]:.0f}</b> 🚽<b style='font-size:10px'>BANHEIROS.</b>""", unsafe_allow_html=True)
                     st.markdown(f"""<b style='font-size:20px'>{bd['vaga'].loc[cont]:.0f}</b> 🚘<b style='font-size:10px'>VAGAS.</b>""", unsafe_allow_html=True)
-                    st.page_link(label='Visitar', page='https://linktr.ee/imoveisfelipecarlos?fbclid=PAZXh0bgNhZW0CMTEAAaY0dJ3FL0eO8Z8ja4Yva-1PmZjEcyrmoAhD-599O3C1mekzS1po-Va6jow_aem_dNdNNOYRiW-W4r82YOZO3A', use_container_width=True)
+                    st.page_link(label='VISITAR', page='https://linktr.ee/imoveisfelipecarlos?fbclid=PAZXh0bgNhZW0CMTEAAaY0dJ3FL0eO8Z8ja4Yva-1PmZjEcyrmoAhD-599O3C1mekzS1po-Va6jow_aem_dNdNNOYRiW-W4r82YOZO3A', use_container_width=True)
         cont += 1
 
 
